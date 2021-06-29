7cb583f533eafa306fb138bb0b4dc1c79360850c
DuanLeeWatkins <leeduan19@gmail.com>
2021-06-28 21:21:13 -0400

    updated task and form functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click')
-    }
-    return (
-      <header className="header">
-        <h1>{title}</h1>
-        <Button color="green" text="Add" onClick={onClick} />
-        
-      </header>
-    );
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 
 Header.defaultProps = {
   title: "Task Tracker",
@@ -22,4 +25,4 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-export default Header
+export default Header;
